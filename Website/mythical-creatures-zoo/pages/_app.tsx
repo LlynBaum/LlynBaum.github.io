2c2835e6ad0c36d831aa476638e8bee1161f9b2f
Darki002 <darkizockt16@gmail.com>
2023-03-31 10:50:12 +0200

    head switch to app

--- Website/mythical-creatures-zoo/pages/_app.tsx
+++ Website/mythical-creatures-zoo/pages/_app.tsx
@@ -2,10 +2,18 @@ import Footer from '@/components/footer/footer'
 import Header from '@/components/header/header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Zoo Mythical Creatures" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
       <Header />
       <Component {...pageProps} />
       <Footer />
--- Website/mythical-creatures-zoo/pages/index.tsx
+++ Website/mythical-creatures-zoo/pages/index.tsx
@@ -1,14 +1,6 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Zoo Mythical Creatures" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className='font-sans'>
         <h1>Hello im an example text</h1>
       </main>
