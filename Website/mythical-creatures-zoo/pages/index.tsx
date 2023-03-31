ec3321ab34c39845f2467d775cf4a0066d1b251b
Darki002 <darkizockt16@gmail.com>
2023-03-31 11:04:13 +0200

    body configs

--- Website/mythical-creatures-zoo/pages/index.tsx
+++ Website/mythical-creatures-zoo/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1>Hello im an example text</h1>
+        <h1 className="text-center font-bold text-2xl">Hello im an example text</h1>
       </main>
     </>
   )
