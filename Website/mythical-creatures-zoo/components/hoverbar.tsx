f8bfff14e3faf53cfa704e2c05d980de53089e19
Darki002 <darkizockt16@gmail.com>
2023-04-14 11:06:19 +0200

    hoverbar anfang

--- Website/mythical-creatures-zoo/components/hoverbar.tsx
+++ Website/mythical-creatures-zoo/components/hoverbar.tsx
@@ -7,8 +7,9 @@ interface HoverBarProp{
 const HoverBar = ({HoverBarProp} : {HoverBarProp: HoverBarProp}) => {
 
     return(
-        <div className={"bg-[url(' " + HoverBarProp.backgroundImgSrc + "')]" + " w-screen h-1/3"}>
-            <h2>{HoverBarProp.category}</h2>
+        <div className={"bg-[url(' " + HoverBarProp.backgroundImgSrc + "')]" + " w-screen h-1/3 flex justify-center items-center"}>
+            <h2 className="font-Sedgwick">{HoverBarProp.category}</h2>
+            <img src={HoverBarProp.hoverImgSrc} className="h-full w-1/5"></img>
         </div>
     )
 }
--- Website/mythical-creatures-zoo/pages/index.tsx
+++ Website/mythical-creatures-zoo/pages/index.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className="bg-banner bg-center w-screen h-72 flex flex-col justify-center items-center font-Sedgwick">
+        <div className="bg-banner bg-center w-screen h-80 flex flex-col justify-center items-center font-Sedgwick mb-5">
           <h1 className="text-center font-bold text-4xl text-zinc-100	">Phantastische Tierwelt</h1>
           <h2 className="text-center font-bold text-2xl mt-4 text-zinc-100	">Entdecke die Welt der Mythical Creatures</h2>
         </div>
 
-        <div className="h-96">
+        <div className="h-96 flex flex-col gap-y-6">
         {
           categories.categories.map(c => 
             <HoverBar HoverBarProp={c}></HoverBar>
