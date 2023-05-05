c645f20104c4c288953b953584267cfd0d2a868e
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2023-05-05 13:47:22 +0200

    impressum

---
+++ Website/mythical-creatures-zoo/pages/imprint/index.tsx
@@ -0,0 +1,46 @@
+const Imprint = () => {
+    return(
+        <main>
+            <div className="text-center text-zinc-100">
+                <h1 className="text-3xl font-bold pt-1">Impressum</h1>
+                <br />
+                <br />
+
+                <h2>Verantwortlich für den Inhalt dieser Website:</h2>
+                <p>Llyn Baumann</p>
+                <p>Arbegstrasse 7C</p>
+                <p>8405 Winterthur Seen</p>
+                <p>Schweiz</p>
+                <br />
+
+                <h2>Kontakt:</h2>
+                <p>Telefon: +41 789092778</p>
+                <p>E-Mail: llyn.baumann@gmx.ch</p>
+                <br />
+
+                <h2>Hosting-Provider:</h2>
+                <p>GitHub</p>
+                <p>88 Colin P Kelly Jr St</p>
+                <p>San Francisco, CA 94107</p>
+                <p>USA</p>
+                <br />
+
+                <h2>Haftungshinweis:</h2>
+                <p>Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</p>
+                <br />
+
+                <h2>Urheberrecht:</h2>
+                <p>Die durch den Betreiber dieser Website erstellten Inhalte und Werke auf diesen Seiten unterliegen dem Schweizer Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechts bedürfen der schriftlichen Zustimmung des Betreibers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p>
+                <br />
+
+                <h2>Umsatzsteuer-Identifikationsnummer:</h2>
+                <p>Gemäß § 19 UStG wird keine Umsatzsteuer erhoben.</p>
+                <br />
+
+                <p>Wir weisen darauf hin, dass die unerlaubte Verwendung von Teilen des Impressums oder des Inhalts dieser Website zu rechtlichen Konsequenzen führen kann.</p>
+            </div>
+        </main>
+    )
+}
+
+export default Imprint;
