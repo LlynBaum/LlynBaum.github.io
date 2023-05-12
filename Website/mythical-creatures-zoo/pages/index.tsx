35c5b6000ec11ae5f2f419c412a6f214dad6f082
Darki002 <darkizockt16@gmail.com>
2023-05-12 10:32:46 +0200

    home mq

--- Website/mythical-creatures-zoo/pages/index.tsx
+++ Website/mythical-creatures-zoo/pages/index.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       <main>
         <Banner title="Phantastische Tierwelt" subTitle="Entdecke die Welt der Mythical Creatures" height="20rem"/>        
         <div className="w-screen h-fit flex flex-col justify-center items-center">
-          <div className="h-96 w-5/6 flex justify-center items-center gap-x-6">
+          <div className="h-96 w-5/6 flex justify-center items-center lg:flex-row flex-col gap-6">
             {
               categories.map(c => {
-                return (
-                  <HoverBar key={c.category} HoverBarProp={c} styles={"max-w-1/3 max-h-full"} />
-                )
-              }
+                  return (
+                    <HoverBar key={c.category} HoverBarProp={c} styles={"max-w-1/3 max-h-full"} />
+                  )
+                }
               )
             }
           </div>
