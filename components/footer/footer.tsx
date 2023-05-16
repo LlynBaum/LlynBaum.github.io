88b664773dbddb45a337d1e4b7e7c904bdc7a249
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2023-05-16 11:44:32 +0200

    about start

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -6,6 +6,8 @@ const Footer = () => {
             <div className="flex gap-x-2 ml-3">
                 <Link href="/imprint">Impressum</Link>
             </div>
+            
+            <p className="ml-3"><i>All Informations on this Website are fake and not real</i></p>
         </footer>
     )
 }
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,16 @@
 const About = () => {
     return(
         <main>
-            <h1>About</h1>
+            <div className="flex flex-col">
+                <h1 className="text-center font-bold text-4xl text-zinc-100 my-10">About</h1>
+                <div className="max-w-1/2 ml-12 text-zinc-100">
+                    <address>
+                    Phantastische Tierwelt Zoo<br />
+                    123 Fantasie Boulevard<br />
+                    Zauberland, 54321<br />         
+                    </address>
+                </div>
+            </div>
         </main>
     )
 }
