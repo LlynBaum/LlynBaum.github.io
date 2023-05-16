8481a5f47f4d57057be3cee398ed7734cf70eda5
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2023-05-16 11:51:18 +0200

    About site

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -10,6 +10,12 @@ const About = () => {
                     Zauberland, 54321<br />         
                     </address>
                 </div>
+                <div className="max-w-3/4 ml-12 text-zinc-100 mt-10">
+                    <h2 className="font-bold text-2xl text-zinc-100 mt-10 mb-3">Über uns:</h2>
+                    <p className="mb-1">Die Vision des Phantastische Tierwelt Zoos besteht darin, durch die Magie mythischer Kreaturen ein Gefühl von Staunen und Phantasie in unseren Besuchern zu wecken. Unser Ziel ist es, eine Welt zu schaffen, in der die Grenze zwischen Fantasie und Realität verschwimmt und in der Besucher die ehrfurchterregende Majestät von Kreaturen erleben können, die nur in der Welt der Mythen und Legenden existieren.</p>
+                    <p className="mb-1">Unser Ziel ist es, eine sichere und bereichernde Umgebung für unsere Tiere zu schaffen, in der sie gedeihen und für die einzigartigen und wundersamen Wesen geschätzt werden, die sie sind. Wir streben danach, führend in der Pflege und Erhaltung mythischer Kreaturen zu sein und arbeiten daran, ihr Überleben für kommende Generationen zu sichern.</p>
+                    <p className="mb-1">Bei Phantastische Tierwelt glauben wir, dass wir durch die Förderung von Liebe und Respekt für diese unglaublichen Wesen eine größere Wertschätzung für die Natur als Ganzes fördern können. Unsere Vision besteht darin, ein Gefühl der Verbindung und Verantwortung für den Planeten und seine Bewohner, sowohl real als auch imaginär, zu inspirieren.</p>
+                </div>
             </div>
         </main>
     )
