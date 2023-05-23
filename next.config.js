31f561a473fa579ad3251a6cce0e046f12ca097d
Darki002 <darkizockt16@gmail.com>
2023-05-23 15:52:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_PATH; 
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: baseUrl,
+  assetPrefix: baseUrl
 }
 
 module.exports = nextConfig
