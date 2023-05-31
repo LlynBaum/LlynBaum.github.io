366f0300a06e90bd37ded2334ba0bb4b17f3565b
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2023-05-31 09:58:40 +0200

    footer color

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Footer = () => {
     return(
         <footer className="h-18 font-sans bg-black/50">
-            <div className="flex gap-x-2 ml-3">
+            <div className="flex gap-x-2 ml-3 text-slate-50">
                 <Link href="/imprint">Impressum</Link>
             </div>
             
-            <p className="ml-3"><i>All Informations on this Website are fake and not real</i></p>
+            <p className="ml-3 text-slate-50"><i>All Informations on this Website are fake and not real</i></p>
         </footer>
     )
 }
