51bc28a66fe9cd110bbdee1758c1d07ab66345ab
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2023-06-02 09:25:17 +0200

    missing header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,9 +3,9 @@ import Navbar from "./navbar/navbar";
 const Header = () => {
     return(
         <>
-            <div className="h-18 w-screen flex justify-center items-center bg-black/50 text-slate-50 font-Sedgwick">
+            <header className="h-18 w-screen flex justify-center items-center bg-black/50 text-slate-50 font-Sedgwick">
                     <Navbar />
-            </div>
+            </header>
         </>
     )
 }
